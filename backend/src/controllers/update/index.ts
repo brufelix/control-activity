93b8990ffee648fa2eab86705d12cd53e2466352
Bruno Felix <bruno.felix@insightlab.ufc.br>
2021-05-11 16:41:25 -0300

    feat: adding update update service

--- backend/src/controllers/update/index.ts
+++ backend/src/controllers/update/index.ts
@@ -1,6 +1,23 @@
 import { Response, Request } from "express";
 import ModelActivity from "../../model";
 
-export default (res: Response, req: Request) => {
-  // get list
-}
+export default (req: Request, res: Response) => {
+  const { _id, description } = req.body;
+
+  ModelActivity.updateOne({ _id }, {
+    $set: { description: description }
+  }, null, (err, _) => {
+    if (err) {
+      res.status(500).send({ err });
+    };
+
+    try {
+      res.status(200).json({
+        code: 200,
+        message: `updated`
+      });
+    } catch (error) {
+      throw new Error(error)
+    };
+  });
+};
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Express } from "express";
 
-import { create, list, remove } from "../controllers";
+import { create, list, remove, update } from "../controllers";
 
 export default (app: Express) => {
   app.get("/api", list);
   app.post("/api", create);
   app.post("/api/delete", remove);
+  app.put("/api/update", update);
 };
