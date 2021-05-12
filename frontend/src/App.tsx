91505f5058f25931f9a71bf68b9f0b5a0cc8c2ce
Bruno Felix <bruno.felix@insightlab.ufc.br>
2021-05-12 16:01:02 -0300

    feat: adding campus field

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Input, Badge, Row } from 'antd';
 
 import Home from "./pages/Home";
 import './App.css';
 
 function App() {
 
+  const { Search } = Input;
   const { Header, Footer, Content } = Layout;
 
   return (
     <Layout>
-      <Header>Header</Header>
-      <Content>
+      <Header>
+        <Row
+          justify="space-between"
+          align="middle"
+        >
+          <Search
+            style={{
+              maxWidth: "300px",
+              marginTop: "20px",
+            }}
+          />
+          <Badge
+            count={12}
+          />
+        </Row>
+      </Header>
+      <Content
+        style={{ overflow: "auto" }}
+      >
         <Home />
       </Content>
-      <Footer>Footer</Footer>
+      <Footer>
+        Footer
+      </Footer>
     </Layout>
   );
 }
