8a2f648c81967e46aa15156f252f5abedcbe7587
Bruno Felix <bruno.felix@insightlab.ufc.br>
2021-05-12 14:48:44 -0300

    feat: performing communication to insert new activity

--- frontend/src/components/CardContainer/index.tsx
+++ frontend/src/components/CardContainer/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Row, Col, Typography, Button, Input } from "antd";
+import { Row, Col, Typography, Button, Input, Modal, } from "antd";
 import { PlusCircleFilled } from "@ant-design/icons";
 
 import api from "../../service";
@@ -10,9 +10,22 @@ import "./index.css";
 const CardContainer: React.FC<ICardContainer> = (props) => {
 
   const { Title } = Typography;
+  const { TextArea } = Input;
 
   const [inputVisible, setInputVisible] = useState(Boolean);
   const [description, setDescription] = useState(String);
+  const [showRegister, setShowRegister] = useState(Boolean);
+  const [modalDescription, setModalDescription] = useState(String);
+
+  const registerActivity = () => {
+    api.post("/activity", {
+      groupId: props._id,
+      description: modalDescription
+    })
+      .then(() => props.fetchData && props.fetchData())
+      .then(() => setShowRegister(false))
+      .then(() => setModalDescription(""));
+  };
 
   const handleEnterInput = () => {
     api.post("/group/update", { _id: props._id, newtitle: description })
@@ -22,79 +35,114 @@ const CardContainer: React.FC<ICardContainer> = (props) => {
   };
 
   return (
-    <Row
-      className="container-card"
-      justify="center"
-    >
-      <Col
-        span={24}
-      >
-        <Row
-          justify="center"
-          align="middle"
-          style={{
-            backgroundColor: "blue",
-            padding: "10px",
-          }}
-        >
-          {
-            inputVisible && (
-              <Input
-                placeholder="Digite o título do grupo..."
-                onPressEnter={() => handleEnterInput()}
-                onChange={(event) => setDescription(event.target.value)}
-                value={description}
-                style={{ maxWidth: 250, }}
-              />
-            )
-          }
-          {
-            !inputVisible && (
-              <Title
-                onClick={() => setInputVisible(true)}
-                level={3}
-                style={{
-                  color: "white",
-                  marginBottom: 0
-                }}
+    <>
+      {
+        showRegister &&
+        (
+          <Modal
+            title={"Cadastro de Atividade"}
+            visible={showRegister}
+            onCancel={() => setShowRegister(false)}
+            footer={[
+              <Button
+                key={0}
+                type="default"
+                onClick={() => setShowRegister(false)}
               >
-                {props.title}
-              </Title>
-            )
-          }
-        </Row>
-        <Row
-          align="middle"
-          justify="center"
-          gutter={[0, 8]}
-          style={{
-            backgroundColor: "#DDD"
-          }}
-        >
-          <Col
-            span={20}
+                Cancelar
+            </Button>,
+              <Button
+                key={1}
+                onClick={() => registerActivity()}
+                type="primary"
+              >
+                Cadastrar
+            </Button>
+            ]}
+            width={400}
           >
-            {
-              props.activities.map((card, index) => (
-                <Card
-                  key={index}
-                  description={card.description}
-                />
-              ))
-            }
-          </Col>
-          <Button
-            onClick={() => props.setShowRegister && props.setShowRegister()}
-            icon={<PlusCircleFilled />}
+            <TextArea
+              onChange={(e) => setModalDescription(e.target.value)}
+              placeholder="Digite a descrição da atividade..."
+              rows={2}
+            />
+          </Modal>
+        )
+      }
+      <Row
+        className="container-card"
+        justify="center"
+      >
+        <Col
+          span={24}
+        >
+          <Row
+            justify="center"
+            align="middle"
             style={{
-              marginBottom: "10px",
+              backgroundColor: "blue",
+              padding: "10px",
             }}
           >
-            Nova Card
+            {
+              inputVisible && (
+                <Input
+                  placeholder="Digite o título do grupo..."
+                  onPressEnter={() => handleEnterInput()}
+                  onChange={(event) => setDescription(event.target.value)}
+                  value={description}
+                  style={{ maxWidth: 250, }}
+                />
+              )
+            }
+            {
+              !inputVisible && (
+                <Title
+                  onClick={() => setInputVisible(true)}
+                  level={3}
+                  style={{
+                    color: "white",
+                    marginBottom: 0
+                  }}
+                >
+                  {props.title}
+                </Title>
+              )
+            }
+          </Row>
+          <Row
+            align="middle"
+            justify="center"
+            gutter={[0, 8]}
+            style={{
+              backgroundColor: "#DDD"
+            }}
+          >
+            <Col
+              span={20}
+            >
+              {
+                props.activities.map((card, index) => (
+                  <Card
+                    key={index}
+                    description={card.description}
+                  />
+                ))
+              }
+            </Col>
+            <Button
+              onClick={() => setShowRegister(true)}
+              icon={<PlusCircleFilled />}
+              style={{
+                marginBottom: "10px",
+              }}
+            >
+              Nova Card
           </Button>
-        </Row>
-      </Col>
-    </Row>
+          </Row>
+        </Col>
+      </Row>
+    </>
   );
 };
 
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Row, Modal, Input, Button } from "antd";
+import { Row, Input, Button } from "antd";
 import { PlusCircleFilled } from "@ant-design/icons";
 
 import api from "../../service";
@@ -8,17 +8,9 @@ import CardContainer from "../../components/CardContainer";
 
 const Home: React.FC = () => {
 
-  const { TextArea } = Input;
-
-  const [showRegister, setShowRegister] = useState(Boolean);
   const [inputVisible, setInputVisible] = useState(Boolean);
   const [description, setDescription] = useState(String);
-  const [modalDescription, setModalDescription] = useState(String);
   const [data, setData] = useState<[ICardContainer]>([{} as ICardContainer]);
-
-  const registerActivity = () => {
-    console.log(modalDescription);
-  };
 
   const handleEnterInput = () => {
     api.post("/group", { title: description })
@@ -29,10 +21,8 @@ const Home: React.FC = () => {
 
   const fetchData = () => {
     api.get("/group")
-      .then(({ data = [] }) => {
-        console.log(data);
-        setData(data.data)
-      });
+      .then(({ data = [] }) => setData(data.data)
+      );
   };
 
   useEffect(() => {
@@ -41,39 +31,6 @@ const Home: React.FC = () => {
 
   return (
     <>
-      {
-        showRegister &&
-        (
-          <Modal
-            title={"Cadastro de Atividade"}
-            visible={showRegister}
-            onCancel={() => setShowRegister(false)}
-            footer={[
-              <Button
-                key={0}
-                type="default"
-                onClick={() => setShowRegister(false)}
-              >
-                Cancelar
-              </Button>,
-              <Button
-                key={1}
-                onClick={() => registerActivity()}
-                type="primary"
-              >
-                Cadastrar
-              </Button>
-            ]}
-            width={400}
-          >
-            <TextArea
-              onChange={(e) => setModalDescription(e.target.value)}
-              placeholder="Digite a descrição da atividade..."
-              rows={2}
-            />
-          </Modal>
-        )
-      }
       <Row
         gutter={[0, 8]}
         align="top"
@@ -91,7 +48,6 @@ const Home: React.FC = () => {
                   _id={item._id}
                   title={item.title}
                   activities={item.activities || []}
-                  setShowRegister={() => setShowRegister(true)}
                   fetchData={() => fetchData()}
                 />
               </div>
