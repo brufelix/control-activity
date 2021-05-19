09c57371969776d0670db0855edfe36501d016d9
Bruno Felix <bruno.felix@insightlab.ufc.br>
2021-05-19 00:10:59 -0300

    refactor: adding format date function

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -5,3 +5,16 @@ export const compareDate = (date: string = "") => {
 
   return now > moment(date);
 };
+
+export const formatDate = (date: string) => {
+  const [year, mon, day] = date.slice(0, 10).split("-")
+  moment.locale('pt');
+  moment.updateLocale('pt', {
+    months: [
+      "Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul",
+      "Ago", "Set", "Out", "Nov", "Dez"
+    ]
+  });
+
+  return moment([year, `${+mon - 1}`, day]).format('DD [de] MMMM/YYYY');
+};
