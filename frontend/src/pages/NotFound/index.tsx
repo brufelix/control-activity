695c839dedae0e42f185e3390b2b5b90c4f411f3
Bruno Felix <brunofelix.ml@gmail.com>
2021-06-26 15:17:19 -0300

    feat: adding page not found

---
+++ frontend/src/pages/NotFound/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Result, Button } from 'antd';
+
+const NotAuthorized: React.FC = () => {
+    return (
+        <Result
+            status="404"
+            title="404"
+            subTitle="Desculpe, esta página não existe."
+            extra={<Button type="primary" href="/login">Voltar</Button>}
+        />
+    );
+}
+
+export default NotAuthorized;
