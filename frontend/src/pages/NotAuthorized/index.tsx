cf6b99b9a548da743b6f8b010c456570a2a355f5
Bruno Felix <brunofelix.ml@gmail.com>
2021-06-26 15:18:03 -0300

    feat: adding unauthorized page

---
+++ frontend/src/pages/NotAuthorized/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Result, Button } from 'antd';
+
+const NotAuthorized: React.FC = () => {
+    return (
+        <Result
+            status="403"
+            title="403"
+            subTitle="Desculpe, você não está autorizado a acessar esta página."
+            extra={<Button type="primary" href="/">Voltar</Button>}
+        />
+    );
+}
+
+export default NotAuthorized;
