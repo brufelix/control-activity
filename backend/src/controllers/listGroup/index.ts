79c2d47be902b4e0f11b581e95c34e0fb963b3a5
Bruno Felix <brunofelix.ml@gmail.com>
2021-07-11 16:23:40 -0300

    feat: adding list group service

--- backend/src/controllers/listGroup/index.ts
+++ backend/src/controllers/listGroup/index.ts
@@ -3,18 +3,16 @@ import { ModelGroup } from "../../model";
 
 export default async (req: Request, res: Response) => {
 
-  ModelGroup.find((err, result) => {
-    if (err) {
-      res.status(404).send();
-    };
+  const { idProject } = req.body;
 
-    try {
+  try {
+    ModelGroup.find({ idProject }, (err, result) => {
+      if (err)
+        res.status(404).send();
+
       res.status(200).send(result);
-    } catch (error) {
-      res.status(500).send({
-        code: 500,
-        message: error
-      });
-    }
-  });
+    });
+  } catch (error) {
+    res.status(500).send({ code: 500, message: error });
+  }
 };
