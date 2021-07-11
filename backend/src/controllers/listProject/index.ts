bdb98f8022f4483a580481fabe01e04f0d8c4280
Bruno Felix <brunofelix.ml@gmail.com>
2021-07-11 16:23:07 -0300

    feat: adding list project service

--- backend/src/controllers/listProject/index.ts
+++ backend/src/controllers/listProject/index.ts
@@ -1,31 +1,32 @@
 import { Response, Request } from "express";
+import { IProject } from "../../interface";
 import { ModelProject, ModelUser } from "../../model";
 
 export default async (req: Request, res: Response) => {
 
-    const { username } = req.body;
     try {
-        let user: any = await ModelUser.find({ username }, (err, user) => {
-            if (err) {
+        let user: any = await ModelUser.find({ username: req.body.username }, (err, user) => {
+            if (err)
                 res.status(401).send();
-            };
 
             return user;
         });
 
-        console.log(user);
+        const [username] = user;
+        const { projects } = username;
 
-        if (user[0].projects.length) {
-            const projects: any = [];
+        if (projects.length) {
+            let projects: IProject[][] = [];
 
-            await user.projects.forEach(async (idProject: string) => {
-                await ModelProject.find({ _id: idProject }, (err, result) => {
+            for (let i = 0; i < projects.length; i++) {
+                await ModelProject.find({ _id: projects[i] }, (err, result) => {
                     if (err)
                         return
 
                     projects.push(result);
-                })
-            })
+                });
+            }
+
             res.status(200).send(projects);
         } else {
             res.status(200).send({ message: "there_are_no_linked_projects" })
